9dcbccd7642d2ef84685760ca51d31fe7a09b498
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-03-20 12:12:10 +0530

    setup monorepo structure

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+
+import express from 'express'
+import type { Request, Response } from "express";
+import cookieparser from 'cookie-parser'
+import cors from "cors"
+dotenv.config();
+
+const app = express() 
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cookieparser())
+
+app.get('/ping',(req:Request,res:Response):Response=>{
+    return res.send("Server is Live!")
+})
+
+// const port:number = Number(process.env.PORT) || 5000
+
+app.listen(4000,()=>{
+    console.log(`server is running at http://localhost:4000`)
+
+})
+
+
